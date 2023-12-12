497b5694300dffef8e15207f5e25bbac982cbfa0
Felipe Peter <felipe.peter@vorausrobotik.com>
2023-12-12 13:39:55 +0800

    feat: Use `rich` to print summary

--- src/voraus_template_updater/_schemas.py
+++ src/voraus_template_updater/_schemas.py
@@ -5,15 +5,17 @@ from enum import Enum
 from typing import Optional
 
 from pydantic import BaseModel
-from tabulate import tabulate
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
 
 
 class Status(Enum):
     """The update status a project can be in."""
 
     UP_TO_DATE = "Up to date"
-    UPDATED_THIS_RUN = "Updated this run -> {}"
-    EXISTING_PR = "Existing PR for {} days ({}) -> {}"
+    UPDATED_THIS_RUN = "Updated this run"
+    EXISTING_PR = "Existing PR since {} ({} days)"
 
 
 class PullRequest(BaseModel):
@@ -35,8 +37,10 @@ class Project(BaseModel):
     """Contains information about a Python project that is maintained via a template."""
 
     name: str
+    url: str
     maintainer: str
     default_branch: str
+    template_url: str
     template_branch: str
     old_template_commit: str
     status: Status
@@ -49,52 +53,91 @@ class Summary(BaseModel):
     projects: list[Project] = []
     skipped_projects: list[SkippedProject] = []
 
-    def __str__(self) -> str:
-        """Returns a string representation of this summary."""
-        output = ""
-
+    def print(self) -> None:
+        """Prints the summary."""
         if len(self.projects) > 0:
-            output += _table_of_projects(self.projects)
+            _print_table_of_projects(self.projects)
 
         if len(self.skipped_projects) > 0:
-            output += "\n\n" + _table_of_skipped_projects(self.skipped_projects)
+            print("\n")
+            _print_table_of_skipped_projects(self.skipped_projects)
 
-        return output
 
+def _print_table_of_projects(projects: list[Project]) -> None:
+    table = Table(title="Processed Projects")
+    table.add_column("Maintainer")
+    table.add_column("Projects")
 
-def _table_of_projects(projects: list[Project]) -> str:
-    headers = ("Project", "Maintainer", "Default Branch", "Template Branch", "Status")
-    data: list[tuple] = []
+    projects_by_maintainer = _get_projects_by_maintainer(projects)
 
-    for project in sorted(projects, key=lambda x: x.maintainer):
-        project_data: tuple[str, ...] = (
-            project.name,
-            project.maintainer,
-            project.default_branch,
-            project.template_branch,
+    for maintainer, maintainers_projects in projects_by_maintainer.items():
+        details = Text()
+
+        for project in maintainers_projects:
+            if project.status == Status.UP_TO_DATE:
+                project_status = project.status.value
+                status_color = "green"
+            elif project.status == Status.UPDATED_THIS_RUN:
+                assert project.pull_request is not None
+                project_status = project.status.value
+                status_color = "yellow"
+            else:
+                assert project.pull_request is not None
+                creation_date = datetime.strftime(project.pull_request.date, "%Y-%m-%d")
+                open_since = (datetime.now().replace(tzinfo=None) - project.pull_request.date.replace(tzinfo=None)).days
+
+                project_status = project.status.value.format(creation_date, open_since)
+                status_color = "red"
+
+            details.append("Project:         ")
+            details.append(project.name)
+            details.append("\nURL:             ")
+            details.append(project.url)
+            details.append("\nStatus:          ", status_color)
+            details.append(project_status, status_color)
+
+            if project.status != Status.UP_TO_DATE:
+                assert project.pull_request is not None
+                details.append("\nPull request:    ", status_color)
+                details.append(project.pull_request.url, status_color)
+
+            details.append("\nDefault branch:  ")
+            details.append(project.default_branch)
+            details.append("\nTemplate URL:    ")
+            details.append(project.template_url)
+            details.append("\nTemplate branch: ")
+            details.append(project.template_branch)
+            details.append("\n\n")
+
+        maintainer_color = (
+            "red"
+            if any(map(lambda p: p.status in (Status.UPDATED_THIS_RUN, Status.EXISTING_PR), projects))
+            else "green"
         )
 
-        if project.status == Status.UP_TO_DATE:
-            project_data = project_data + (project.status.value,)
-        elif project.status == Status.UPDATED_THIS_RUN:
-            assert project.pull_request is not None
-            project_data = project_data + (project.status.value.format(project.pull_request.url),)
+        table.add_row(Text(maintainer, maintainer_color), details)
+
+    Console().print(table)
+
+
+def _get_projects_by_maintainer(projects: list[Project]) -> dict[str, list[Project]]:
+    projects_by_maintainer: dict[str, list[Project]] = {}
+
+    for project in sorted(projects, key=lambda x: x.maintainer):
+        if project.maintainer not in projects_by_maintainer:
+            projects_by_maintainer[project.maintainer] = [project]
         else:
-            assert project.pull_request is not None
-            creation_date = datetime.strftime(project.pull_request.date, "%Y-%m-%d")
-            open_since = (datetime.now().replace(tzinfo=None) - project.pull_request.date.replace(tzinfo=None)).days
-
-            project_data = project_data + (
-                project.status.value.format(open_since, creation_date, project.pull_request.url),
-            )
-
-        data.append(project_data)
-
-    return tabulate(data, headers=headers) + "\n"
+            projects_by_maintainer[project.maintainer].append(project)
+    return projects_by_maintainer
 
 
-def _table_of_skipped_projects(projects: list[SkippedProject]) -> str:
-    headers = ("Skipped Project", "Reason", "URL")
-    data = [(p.name, p.reason, p.url) for p in projects]
+def _print_table_of_skipped_projects(projects: list[SkippedProject]) -> None:
+    table = Table(title="Skipped Projects")
+    table.add_column("Project")
+    table.add_column("URL")
+    table.add_column("Reason")
 
-    return tabulate(data, headers=headers) + "\n"
+    for project in projects:
+        table.add_row(project.name, project.url, project.reason)
+
+    Console().print(table)
--- tests/resources/example_summary.py
+++ tests/resources/example_summary.py
@@ -8,24 +8,30 @@ summary = Summary(
     projects=[
         Project(
             name="project-1",
+            url="https://project1.com",
             maintainer="maintainer-1",
             default_branch="master",
+            template_url="https://first-template.com",
             template_branch="main",
             old_template_commit="abc123",
             status=Status.UP_TO_DATE,
         ),
         Project(
             name="project-2",
+            url="https://project2.com",
             maintainer="maintainer-2",
             default_branch="main",
+            template_url="https://second-template.com",
             template_branch="development",
             old_template_commit="abc123",
             status=Status.UP_TO_DATE,
         ),
         Project(
             name="project-3",
+            url="https://project3.com",
             maintainer="maintainer-1",
             default_branch="master",
+            template_url="https://first-template.com",
             template_branch="main",
             old_template_commit="abc123",
             status=Status.UPDATED_THIS_RUN,
@@ -33,8 +39,10 @@ summary = Summary(
         ),
         Project(
             name="project-4",
+            url="https://project4.com",
             maintainer="maintainer-2",
             default_branch="main",
+            template_url="https://second-template.com",
             template_branch="development",
             old_template_commit="abc123",
             status=Status.UPDATED_THIS_RUN,
@@ -42,8 +50,10 @@ summary = Summary(
         ),
         Project(
             name="project-5",
+            url="https://project5.com",
             maintainer="maintainer-1",
             default_branch="main",
+            template_url="https://first-template.com",
             template_branch="main",
             old_template_commit="abc123",
             status=Status.EXISTING_PR,
@@ -51,12 +61,24 @@ summary = Summary(
         ),
         Project(
             name="project-6",
+            url="https://project6.com",
             maintainer="maintainer-2",
             default_branch="main",
+            template_url="https://second-template.com",
             template_branch="development",
             old_template_commit="abc123",
             status=Status.EXISTING_PR,
             pull_request=PullRequest(url="https://pr6.com", date=datetime(2023, 12, 12)),
+        ),
+        Project(
+            name="project-7",
+            url="https://project7.com",
+            maintainer="maintainer-3",
+            default_branch="main",
+            template_url="https://second-template.com",
+            template_branch="development",
+            old_template_commit="abc123",
+            status=Status.UP_TO_DATE,
         ),
     ],
     skipped_projects=[
@@ -64,3 +86,6 @@ summary = Summary(
         SkippedProject(name="project-10", url="https://project10.com", reason="Project archived"),
     ],
 )
+
+# Uncomment to see what would get printed to the console
+# summary.print()
